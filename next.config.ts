00b5b240e558578f34c2c737fa2fdf402afd3c24
Brian <brianpk07@gmail.com>
2026-05-31 18:12:37 +0200

    18h12/31/05/2026

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // <-- Ajoutez aussi ça pour éviter les bugs d'images sur GitHub
   },
+  basePath: '/portfolio-v2', // <-- AJOUTE CETTE LIGNE (avec le nom exact de ton dossier GitHub)
 };
 
 export default nextConfig;
